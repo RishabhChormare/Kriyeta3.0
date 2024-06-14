61ad55b21b047d7cd9459d42707e1c23b2108155
yashikasharma04 <yashika12180@gmail.com>
2024-06-14 20:35:20 +0530

    123

--- ArogyaChetna/ArogyaChetna/settings.py
+++ ArogyaChetna/ArogyaChetna/settings.py
@@ -115,8 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
 
+STATIC_URL = '/static/'
+
+SAICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/photo/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/photo/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- ArogyaChetna/Niramaya/views.py
+++ ArogyaChetna/Niramaya/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.  
+def index(request):
+    return render(request, 'index.html')
+
+def doctor_nurse(request):
+    return render(request, 'doctor_nurse.html')
+
+def admin(request):
+    return render(request, 'admin.html')
+
+def about(request):
+    return render(request, 'about.html')
